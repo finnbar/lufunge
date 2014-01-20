cebf867ab8bf17ff742f37b82c8a3dd476d7c4d5
finnbar <finnjkeating@aol.com>
2014-01-20 21:12:58 +0000

    Moved code into a separate place, added scaling and stuff. TODO: magnifying mouse

--- main.lua
+++ main.lua
@@ -1,114 +1,128 @@
+--todo: magnification when mouseovered
+
 require("lufunge")
-_deja = love.graphics.newFont("DejaVuSans.ttf")
 _dejalarge = love.graphics.newFont("DejaVuSans.ttf",30)
-_play,_ok,_err,_print,_vars,_prevPointers,_prevPrevs = false,true,"nil",{},0,{},{}
-_code = [[
-~a,b=2,2~
-  v   <        <
-       vb=1+b < 
-  a    > b#a   \
-  .   / b[2$a  <
-@2>+1v>>a%b#0!\ 
-  a  = b        
-     a =        
-     >2^        
-  ^           < 
-~print(a)~]]
--- _code = [[
--- ~a,b=function(p)if p==100 then p=0 end return love.math.random(p,100) end,0~
---   v       <
---   v b=0  < 
--- @1>a;#b  \b
--- @1>a=b;.v>^
---   ^     <
--- ]]
+_dejasys = love.graphics.newFont("DejaVuSans.ttf")
+_play,_ok,_err,_print,_vars = false,true,"nil",{},0
+_slow,_slowCount = false,0.1
+_code = love.filesystem.read("code.lufunge")
+_size = 40
+_superSpeed = 10000
 
 function love.load()
-	love.graphics.setFont(_deja)
-  lufungeInit(_code,true)
+	lufungeInit(_code,true)
+	local sx = 11/_height
+	local sy = 16/_width
+	print("scale:",sx,sy)
+	local hs
+	if sx<sy then hs=sx else hs=sy end
+	_size=40*hs
+	_deja = love.graphics.newFont("DejaVuSans.ttf",12*hs)
 end
 
 function love.draw()
-  love.graphics.setColor(255,255,255)
-  love.graphics.print("lufunge interpreter",320,4)
-  if love.mouse.getX()>0 and love.mouse.getX()<15 and love.mouse.getY()>0 and love.mouse.getY()<15 then love.graphics.setColor(255,0,0) end   
-  love.graphics.print("X",4,4)
-  love.graphics.setColor(255,255,255)
-  if love.mouse.getX()>14 and love.mouse.getX()<30 and love.mouse.getY()>0 and love.mouse.getY()<25 then love.graphics.setColor(0,255,0) end
-  love.graphics.print("▷",15,3)
-  love.graphics.setColor(255,255,255)
-  for p in pairs(_pointers) do
-    love.graphics.setColor(_cols[p%6])
-    love.graphics.rectangle("fill", _pointers[p].x*40-20,_pointers[p].y*40,40,40)
-    love.graphics.print(tostring(_pointers[p].val)..", "..tostring(_pointers[p].dir)..", "..tostring(t[_pointers[p].y][_pointers[p].x]),(120*p)-110,600)
-  end
-  for p in pairs(_prevPointers) do
-    love.graphics.setColor({_cols[p%6][1],_cols[p%6][2],_cols[p%6][3],125})
-    love.graphics.rectangle("fill", _prevPointers[p].x*40-20,_prevPointers[p].y*40,40,40)
-  end
-  for p in pairs(_prevPrevs) do
-    love.graphics.setColor({_cols[p%6][1],_cols[p%6][2],_cols[p%6][3],65})
-    love.graphics.rectangle("fill", _prevPrevs[p].x*40-20,_prevPrevs[p].y*40,40,40)
-  end
-  love.graphics.setColor(255,255,255,255)
-  if t~=nil then
-    for y=1,_height do
-      for x=1,#t[y] do
-        love.graphics.print(t[y][x],x*40,(y*40)+20)
-      end
-    end
-  end
-  for y=1,_height do
-    for x=1,_width,1 do
-      love.graphics.rectangle("line", x*40-20, y*40, 40, 40)
-    end
-  end
-  local c,prev,val=2,"",0
-  love.graphics.setColor(255,255,255)
-  _vars=0
-  for i,v in pairs(_G) do
-  	if type(v)~="table" and type(v)~="function" then
-  		if not tostring(i):find("^_") then
-  			if prev~="" then
-  				local var1,var2
-  				if type(v)=="string" then
-  					var1=i.."=\""..tostring(v).."\""
-  				else var1=i.."="..tostring(v) end
-  				if type(val)=="string" then
-  					var2=prev.."=\""..tostring(val).."\""
-  				else var2=prev.."="..tostring(val) end
-		  		love.graphics.print(var1.." "..var2,670,(c*20))
-		  		c=c+1
-		  		prev=""
-		  		val=0
-		  	else prev=i val=v end
-	  		_vars=_vars+1
-	  	end
-  	end
-  end
-  love.graphics.print("print:",670,(c*20))
-  c=c+1
-  for i=#_print,1,-1 do
-  	love.graphics.print(tostring(_print[i]),670,(c*20))
-  	c=c+1
-  end
-  if not _ok then
-  	_play=false
-  	love.graphics.setColor(0,0,0)
-  	love.graphics.rectangle("fill", 10, 100, 660, 120)
-  	love.graphics.setColor(255,255,255)
-  	love.graphics.setFont(_dejalarge)
-  	love.graphics.printf("AN ERROR WAS RAISED: ".._err,20,100,650,"center")
-  end
-  love.graphics.setFont(_deja)
+	love.graphics.setFont(_dejasys)
+	love.graphics.setColor(255,255,255)
+	love.graphics.print("lufunge interpreter",320,4)
+	if love.mouse.getX()>0 and love.mouse.getX()<15 and love.mouse.getY()>0 and love.mouse.getY()<15 then love.graphics.setColor(255,0,0) end   
+	love.graphics.print("X",4,4)
+	love.graphics.setColor(255,255,255)
+	if love.mouse.getX()>14 and love.mouse.getX()<30 and love.mouse.getY()>0 and love.mouse.getY()<25 then love.graphics.setColor(0,255,0) end
+	love.graphics.print("▷",15,3)
+	love.graphics.setColor(255,255,255)
+	if love.mouse.getX()>29 and love.mouse.getX()<40 and love.mouse.getY()>0 and love.mouse.getY()<25 then love.graphics.setColor(255,255,0) end
+	love.graphics.print("+",26,3)
+	love.graphics.setColor(255,255,255)
+	if love.mouse.getX()>39 and love.mouse.getX()<50 and love.mouse.getY()>0 and love.mouse.getY()<25 then love.graphics.setColor(0,255,255) end
+	love.graphics.print("-",37,3)
+	love.graphics.setColor(255,255,255)
+	love.graphics.setFont(_deja)
+	for p in pairs(_pointers) do
+		love.graphics.setFont(_deja)
+		love.graphics.setColor(_cols[p%6])
+		love.graphics.rectangle("fill", _pointers[p].x*_size-(_size/2),_pointers[p].y*_size,_size,_size)
+		love.graphics.setFont(_dejasys)
+		love.graphics.print(tostring(_pointers[p].val)..", "..tostring(_pointers[p].dir)..", "..tostring(t[_pointers[p].y][_pointers[p].x]),(120*p)-110,600)
+	end
+	love.graphics.setFont(_deja)
+	for p in pairs(_prevPointers) do
+		love.graphics.setColor({_cols[p%6][1],_cols[p%6][2],_cols[p%6][3],125})
+		love.graphics.rectangle("fill", _prevPointers[p].x*_size-(_size/2),_prevPointers[p].y*_size,_size,_size)
+	end
+	for p in pairs(_prevPrevs) do
+		love.graphics.setColor({_cols[p%6][1],_cols[p%6][2],_cols[p%6][3],65})
+		love.graphics.rectangle("fill", _prevPrevs[p].x*_size-(_size/2),_prevPrevs[p].y*_size,_size,_size)
+	end
+	love.graphics.setColor(255,255,255,255)
+	if t~=nil then
+		for y=1,_height do
+			for x=1,#t[y] do
+				love.graphics.print(t[y][x],x*_size,(y*_size)+(_size/2))
+			end
+		end
+	end
+	for y=1,_height do
+		for x=1,_width,1 do
+			love.graphics.rectangle("line", x*_size-(_size/2), y*_size, _size, _size)
+		end
+	end
+	local c,prev,val=2,"",0
+	love.graphics.setColor(255,255,255)
+	_vars=0
+	love.graphics.setFont(_dejasys)
+	for i,v in pairs(_G) do
+		if type(v)~="table" and type(v)~="function" then
+			if not tostring(i):find("^_") then
+				if prev~="" then
+					local var1,var2
+					if type(v)=="string" then
+						var1=i.."=\""..tostring(v).."\""
+					else var1=i.."="..tostring(v) end
+					if type(val)=="string" then
+						var2=prev.."=\""..tostring(val).."\""
+					else var2=prev.."="..tostring(val) end
+					love.graphics.print(var1.." "..var2,670,(c*20))
+					c=c+1
+					prev=""
+					val=0
+				else prev=i val=v end
+				_vars=_vars+1
+			end
+		end
+	end
+	love.graphics.print("print:",670,(c*20))
+	c=c+1
+	for i=#_print,1,-1 do
+		love.graphics.print(tostring(_print[i]),670,(c*20))
+		c=c+1
+	end
+	if not _ok then
+		_play=false
+		love.graphics.setColor(0,0,0)
+		love.graphics.rectangle("fill", 10, 100, 660, 120)
+		love.graphics.setColor(255,255,255)
+		love.graphics.setFont(_dejalarge)
+		love.graphics.printf("AN ERROR WAS RAISED: ".._err,20,100,650,"center")
+	end
+	love.graphics.setFont(_deja)
 end
 
 function love.update(dt)
-	if _play then 
-		_ok,_err = pcall(lufungeStep)
+	if _play then
+		for x=1,_superSpeed do
+			_ok,_err = pcall(lufungeStep)
+			if not _ok then break end
+			if #_print>(21-_vars) then
+				table.remove(_print,1)
+			end
+		end
 	end
-	if #_print>(21-_vars) then
-		table.remove(_print,1)
+	if _slow then
+		if _slowCount<=0 then
+			_ok,_err = pcall(lufungeStep)
+			_slowCount=0.1
+		else _slowCount=_slowCount-dt
+		end
 	end
 end
 
@@ -118,5 +132,28 @@ function love.mousepressed(x, y, button)
 	elseif button=="l" and x<30 and x>14 and y<15 and y>0 then
 		_play=not _play
 		if _play and _err then lufungeInit(_code,true) end
+	elseif button=="l" and x<40 and x>29 and y<15 and y>0 then
+		if _superSpeed<1000000 then _superSpeed=_superSpeed*10 end
+	elseif button=="l" and x<55 and x>39 and y<15 and y>0 then
+		if _superSpeed>1 then _superSpeed=_superSpeed/10 end
 	elseif button=="l" then _ok=true end
+end
+
+function love.keypressed(key, isrepeat)
+	if key==" " then
+		_play=not _play
+		if _play and _err then lufungeInit(_code,true) end
+	elseif key=="return" then
+		_slow=true
+	elseif key=="+" then
+		if _superSpeed<1000000 then _superSpeed=_superSpeed*10 end
+	elseif key=="-" then
+		if _superSpeed>1 then _superSpeed=_superSpeed/10 end
+	end
+end
+
+function love.keyreleased(key)
+	if key=="return" then
+		_slow=false
+	end
 end
