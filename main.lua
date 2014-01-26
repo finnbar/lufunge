b86d0ca4fad14a28b995ee95f1fa501e22e53b32
finnbar <finnjkeating@aol.com>
2014-01-26 17:22:06 +0000

    Added gamera zooming and finally code editing! And saving to .local!

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
---todo: magnification when mouseovered
-
 require("lufunge")
+local gamera = require("gamera")
+local cam = gamera.new(0,0,800,620)
 _dejalarge = love.graphics.newFont("DejaVuSans.ttf",30)
 _dejasys = love.graphics.newFont("DejaVuSans.ttf")
 _play,_ok,_err,_print,_vars = false,true,"nil",{},0
@@ -8,6 +8,7 @@ _slow,_slowCount = false,0.1
 _code = love.filesystem.read("code.lufunge")
 _size = 40
 _superSpeed = 10000
+_zoom = 1
 
 function love.load()
 	lufungeInit(_code,true)
@@ -21,6 +22,8 @@ function love.load()
 end
 
 function love.draw()
+	cam:setPosition(love.mouse.getX(),love.mouse.getY())
+	cam:setScale(_zoom)
 	love.graphics.setFont(_dejasys)
 	love.graphics.setColor(255,255,255)
 	love.graphics.print("lufunge interpreter",320,4)
@@ -35,37 +38,43 @@ function love.draw()
 	love.graphics.setColor(255,255,255)
 	if love.mouse.getX()>39 and love.mouse.getX()<50 and love.mouse.getY()>0 and love.mouse.getY()<25 then love.graphics.setColor(0,255,255) end
 	love.graphics.print("-",37,3)
-	love.graphics.setColor(255,255,255)
-	love.graphics.setFont(_deja)
-	for p in pairs(_pointers) do
+	cam:draw(function()
+		if _xp~=nil and _yp~=nil then
+			--love.graphics.circle("fill",_xp*_size,_yp*_size,5)
+			love.graphics.polygon("fill",{_xp*_size,_yp*_size+10,_xp*_size-5,_yp*_size-5,_xp*_size+5,_yp*_size-5})
+		end
+		love.graphics.setColor(255,255,255)
 		love.graphics.setFont(_deja)
-		love.graphics.setColor(_cols[p%6])
-		love.graphics.rectangle("fill", _pointers[p].x*_size-(_size/2),_pointers[p].y*_size,_size,_size)
-		love.graphics.setFont(_dejasys)
-		love.graphics.print(tostring(_pointers[p].val)..", "..tostring(_pointers[p].dir)..", "..tostring(t[_pointers[p].y][_pointers[p].x]),(120*p)-110,600)
-	end
-	love.graphics.setFont(_deja)
-	for p in pairs(_prevPointers) do
-		love.graphics.setColor({_cols[p%6][1],_cols[p%6][2],_cols[p%6][3],125})
-		love.graphics.rectangle("fill", _prevPointers[p].x*_size-(_size/2),_prevPointers[p].y*_size,_size,_size)
-	end
-	for p in pairs(_prevPrevs) do
-		love.graphics.setColor({_cols[p%6][1],_cols[p%6][2],_cols[p%6][3],65})
-		love.graphics.rectangle("fill", _prevPrevs[p].x*_size-(_size/2),_prevPrevs[p].y*_size,_size,_size)
-	end
-	love.graphics.setColor(255,255,255,255)
-	if t~=nil then
-		for y=1,_height do
-			for x=1,#t[y] do
-				love.graphics.print(t[y][x],x*_size,(y*_size)+(_size/2))
+		for p in pairs(_pointers) do
+			love.graphics.setFont(_deja)
+			love.graphics.setColor(_cols[p%6])
+			love.graphics.rectangle("fill", _pointers[p].x*_size-(_size/2),_pointers[p].y*_size,_size,_size)
+			love.graphics.setFont(_dejasys)
+			love.graphics.print(tostring(_pointers[p].val)..", "..tostring(_pointers[p].dir)..", "..tostring(t[_pointers[p].y][_pointers[p].x]),(120*p)-110,600)
+		end
+		love.graphics.setFont(_deja)
+		for p in pairs(_prevPointers) do
+			love.graphics.setColor({_cols[p%6][1],_cols[p%6][2],_cols[p%6][3],125})
+			love.graphics.rectangle("fill", _prevPointers[p].x*_size-(_size/2),_prevPointers[p].y*_size,_size,_size)
+		end
+		for p in pairs(_prevPrevs) do
+			love.graphics.setColor({_cols[p%6][1],_cols[p%6][2],_cols[p%6][3],65})
+			love.graphics.rectangle("fill", _prevPrevs[p].x*_size-(_size/2),_prevPrevs[p].y*_size,_size,_size)
+		end
+		love.graphics.setColor(255,255,255,255)
+		if t~=nil then
+			for y=1,_height do
+				for x=1,#t[y] do
+					love.graphics.print(t[y][x],x*_size,(y*_size)+(_size/2))
+				end
 			end
 		end
-	end
-	for y=1,_height do
-		for x=1,_width,1 do
-			love.graphics.rectangle("line", x*_size-(_size/2), y*_size, _size, _size)
+		for y=1,_height do
+			for x=1,_width,1 do
+				love.graphics.rectangle("line", x*_size-(_size/2), y*_size, _size, _size)
+			end
 		end
-	end
+	end)
 	local c,prev,val=2,"",0
 	love.graphics.setColor(255,255,255)
 	_vars=0
@@ -136,24 +145,39 @@ function love.mousepressed(x, y, button)
 		if _superSpeed<1000000 then _superSpeed=_superSpeed*10 end
 	elseif button=="l" and x<55 and x>39 and y<15 and y>0 then
 		if _superSpeed>1 then _superSpeed=_superSpeed/10 end
-	elseif button=="l" then _ok=true end
+	elseif button=="l" then
+		_ok=true
+		_xp,_yp = cam:toWorld(x,y)
+		_xp=_xp-(_size/2)
+		_xp=math.floor(_xp/_size)+1
+		_yp=math.floor(_yp/_size)
+		if _yp>0 and _yp<_height+1 and _xp>0 and _xp<_width+1 then t[_yp][_xp]="" _code=genCode() end
+	elseif button=="wu" then _zoom=_zoom+0.2
+	elseif button=="wd" then _zoom=_zoom-0.2
+	end
+	if _zoom<1 then _zoom=1 end
 end
 
-function love.keypressed(key, isrepeat)
-	if key==" " then
-		_play=not _play
-		if _play and _err then lufungeInit(_code,true) end
-	elseif key=="return" then
-		_slow=true
-	elseif key=="+" then
-		if _superSpeed<1000000 then _superSpeed=_superSpeed*10 end
-	elseif key=="-" then
-		if _superSpeed>1 then _superSpeed=_superSpeed/10 end
-	end
+function love.textinput(key)
+	if _yp>0 and _yp<_height+1 and _xp>0 and _xp<_width+1 then t[_yp][_xp]=key _code=genCode() end
 end
 
 function love.keyreleased(key)
 	if key=="return" then
 		_slow=false
 	end
+end
+
+function genCode()
+	local newCode=""
+	if t~=nil then
+		for y=1,_height do
+			for x=1,#t[y] do
+				newCode=newCode..t[y][x]
+			end
+			newCode=newCode.."\r"
+		end
+	end
+	love.filesystem.write("code.lufunge", newCode,all)
+	return newCode
 end
